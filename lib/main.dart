52490475e6883d35da2a1dc6481774a2ce269f2d
Fernanda <fernanda.marana44@gmail.com>
2020-07-07 23:40:21 -0300

    Async Data Working

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,72 @@ void main() {
   );
 }
 
+
+class Infos{
+  final bool adult;
+  final String backdrop_url;
+  final Map belongs_to_collection;
+  final int budget;
+  final List genres;
+  final String homepage;
+  final int id;
+  final String imdb_id;
+  final String original_language;
+  final String original_title;
+  final String overview;
+  final double popularity;
+  final String poster_url;
+  final List production_companies;
+  final List production_countries;
+  final String release_date;
+  final int revenue;
+  final int runtime;
+  final List spoken_languages;
+  final String status;
+  final String tagline;
+  final String title;
+  final bool video;
+  final double vote_average;
+  final int vote_count;
+
+  Infos({this.adult, this.backdrop_url, this.belongs_to_collection, this.budget,
+    this.genres, this.homepage, this.id, this.imdb_id, this.original_language, this.original_title,
+    this.overview, this.popularity,this.poster_url, this.production_companies, this.production_countries,
+    this.release_date, this.revenue, this.runtime, this.spoken_languages, this.status, this.tagline,
+    this.title, this.video,this.vote_average, this.vote_count});
+
+  factory Infos.fromJson(Map<String, dynamic> json){
+    return Infos(
+      adult : json['adult'],
+      backdrop_url : json['backdrop_url'],
+      belongs_to_collection : json['belongs_to_collection'],
+      budget : json['budget'],
+      genres : json['genres'],
+      homepage : json['homepage'],
+      id : json['id'],
+      imdb_id : json['imdb_id'],
+      original_language : json['original_language'],
+      original_title : json['original_tile'],
+      overview : json['overview'],
+      popularity: json['popularity'],
+      poster_url : json['poster_url'],
+      production_companies : json['production_companies'],
+      production_countries : json['production_countries'],
+      release_date : json['release_date'],
+      revenue : json['revenue'],
+      runtime : json['runtime'],
+      spoken_languages : json['spoken_languages '],
+      status : json['status'],
+      tagline : json['tagline'],
+      title : json['title'],
+      video : json['video'],
+      vote_average : json['vote_average'],
+      vote_count : json['vote_count'],
+    );
+  }
+
+}
+
 //Scaffold(
 //backgroundColor: Colors.blueGrey[500],
 //appBar: AppBar(
@@ -106,143 +172,79 @@ class NextPage extends StatefulWidget {
 
 class _NextPageState extends State<NextPage> {
 
- // List data;
-  Future<Infos> infos;
+  List data;
+  Future<Infos> _infos;
 
-  Future<Infos> getFutureData() async{
-    print(widget.urls.toString());
-    http.Response response = await http.get(Uri.encodeFull(widget.urls),headers: {"Accept": "application/json"});
-    // if(response.statusCode == 200){
-    print(response);
-    //return
-    await Future.delayed(Duration(seconds: 3));
-    String content = response.body;
-
-    return Infos.fromJson(json.decode(content));
-
-  }
-
-
-  Future getData() async{
-    //infos = await getFutureData();
-  //  getFutureData().then((Infos _info){
-    //  setState(() {
-      //  infos = _info;
-      //});
+  Future<Infos> fetchData() async{
+    http.Response response = await http.get(
+        Uri.encodeFull(widget.urls),
+        headers: {"Accept": "application/json"}
+    );
+    if(response.statusCode == 200){
+      return Infos.fromJson(json.decode(response.body));
+    }else{
+      throw Exception('Failed');
     }
-
-
-
-     // setState(() {
-     //   infos = await Infos.fromJson(json.decode((response.body))));
-      //data = json.decode(response.body);
-
-     // });
-  //  } else {
-   //   throw Exception("NO response from server");
-   // }
-
-
+   // print(response);
+  //  setState(() {
+  //    data = json.decode(response.body);
+  //    print(data.length.toString() );
+  //  });
+ //   return "Sucess";
+  }
 
   @override
   void initState(){
     super.initState();
-    infos = getFutureData();
+    _infos = fetchData();
 
   }
+
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Next Page"),
-      ),
-      body: Center(
-        child: FutureBuilder<Infos>(
-            future: infos,
-            builder: (context,snapshot){
-              if (snapshot.connectionState == ConnectionState.waiting)
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              else
-                return Container(
-                  width: 100,
-                  height: 100,
-                  color: snapshot.hasData ? Colors.green : Colors.red,
-                );
-            }
-          // child: Text(data['id'].toString()),
-        ),
+        title: new Text("Detalhes"),
 
+      ),
+      body: FutureBuilder(
+        future: _infos,
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+            return Center(
+              child: Card(
+                margin: EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Container(
+                      child: Text(snapshot.data.title,
+                      style: TextStyle(fontFamily: 'RobotMono', fontSize: 42),
+                      ),
+                    ),
+                    Container(
+                      child: Image.network(snapshot.data.poster_url),
+                    ),
+                    Container(
+                      child: Text("Overview: " + snapshot.data.overview),
+                    ),
+                  ],
+                ),
+              ),
+
+
+            );
+              //Text(snapshot.data.title);
+          }else if(snapshot.hasError){
+            return Text("${snapshot.error}");
+          }else if(snapshot.hasData == false){
+            return Text("No Data");
+          }
+          return CircularProgressIndicator();
+        }
       )
 
 
     );
   }
-}
-
-class Infos{
-  final bool adult;
-  final String backdrop_url;
-  final String belongs_to_collection;
-  final int budget;
-  final List genres;
-  final String homepage;
-  final int id;
-  final int imdb_id;
-  final String original_language;
-  final String original_title;
-  final String overview;
-  final double popularity;
-  final String poster_url;
-  final List production_companies;
-  final List production_countries;
-  final String release_date;
-  final int revenue;
-  final int runtime;
-  final List spoken_languages;
-  final String status;
-  final String tagline;
-  final String title;
-  final bool video;
-  final double vote_average;
-  final int vote_count;
-
-  Infos({this.adult, this.backdrop_url, this.belongs_to_collection, this.budget,
-    this.genres, this.homepage, this.id, this.imdb_id, this.original_language, this.original_title,
-    this.overview, this.popularity,this.poster_url, this.production_companies, this.production_countries,
-    this.release_date, this.revenue, this.runtime, this.spoken_languages, this.status, this.tagline,
-    this.title, this.video,this.vote_average, this.vote_count});
-
-  factory Infos.fromJson(Map<String, dynamic> json){
-    return Infos(
-      adult : json['adult'],
-      backdrop_url : json['backdrop_url'],
-      belongs_to_collection : json['belongs_to_collection'],
-      budget : json['budget'],
-      genres : json['genres'],
-      homepage : json['homepage'],
-      id : json['id'],
-      imdb_id : json['imdb_id'],
-      original_language : json['original_language'],
-      original_title : json['original_tile'],
-      overview : json['overview'],
-      popularity: json['popularity'],
-      poster_url : json['poster_url'],
-      production_companies : json['production_companies'],
-      production_countries : json['production_countries'],
-      release_date : json['release_date'],
-      revenue : json['revenue'],
-      runtime : json['runtime'],
-      spoken_languages : json['spoken_languages '],
-      status : json['status'],
-      tagline : json['tagline'],
-      title : json['title'],
-      video : json['video'],
-      vote_average : json['vote_average'],
-      vote_count : json['vote_count'],
-    );
-  }
-
 }
